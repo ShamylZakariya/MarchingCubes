21f5b640967d444d182f7d8fcb20f5a452b363bf
Shamyl Zakariya <shamyl@gmail.com>
2019-11-23 15:17:14 -0800

    Create general purpose resizable indexed vertex storage

--- src/main.cpp
+++ src/main.cpp
@@ -53,82 +53,217 @@ struct ProgramState {
     GLuint program = 0;
     GLint uniformLocMVP = -1;
 
+    ~ProgramState()
+    {
+        if (program > 0) {
+            glDeleteProgram(program);
+        }
+    }
+
     bool build(const std::string& vertSrc, const std::string& fragSrc)
     {
         program = util::CreateProgram(vertSrc.c_str(), fragSrc.c_str());
-        if (util::CheckGlError("CreateProgram")) {
-            return false;
-        }
+        util::CheckGlError("CreateProgram");
         uniformLocMVP = glGetUniformLocation(program, "uMVP");
         return uniformLocMVP >= 0;
     }
 };
 
-const std::vector<Vertex> Vertices = {
-    // -Z
-    { { -1, -1, -1 }, { +1, +1, +0 } },
-    { { +1, +1, -1 }, { +1, +1, +0 } },
-    { { +1, -1, -1 }, { +1, +1, +0 } },
+#pragma mark -
 
-    { { -1, -1, -1 }, { +1, +1, +0 } },
-    { { -1, +1, -1 }, { +1, +1, +0 } },
-    { { +1, +1, -1 }, { +1, +1, +0 } },
+class IndexedVertexStorage {
+private:
+    GLuint _vertexVboId = 0;
+    GLuint _indexVboId = 0;
+    GLuint _vao = 0;
+    std::size_t _numIndices = 0;
+    std::size_t _numVertices = 0;
+    float _growthFactor;
 
-    // +X
-    { { +1, -1, -1 }, { +1, +0, +1 } },
-    { { +1, +1, +1 }, { +1, +0, +1 } },
-    { { +1, -1, +1 }, { +1, +0, +1 } },
+public:
+    IndexedVertexStorage(const std::vector<Vertex>& vertices, const std::vector<GLuint>& indices, float growthFactor = 1.5F)
+        : _growthFactor(growthFactor)
+    {
+        update(vertices, indices);
+    }
 
-    { { +1, -1, -1 }, { +1, +0, +1 } },
-    { { +1, +1, -1 }, { +1, +0, +1 } },
-    { { +1, +1, +1 }, { +1, +0, +1 } },
+    ~IndexedVertexStorage()
+    {
+        if (_vao > 0)
+            glDeleteVertexArrays(1, &_vao);
+        if (_vertexVboId > 0)
+            glDeleteBuffers(1, &_vertexVboId);
+        if (_indexVboId > 0)
+            glDeleteBuffers(1, &_indexVboId);
+    }
 
-    // +Z
-    { { +1, -1, +1 }, { +0, +1, +1 } },
-    { { -1, +1, +1 }, { +0, +1, +1 } },
-    { { -1, -1, +1 }, { +0, +1, +1 } },
+    void draw()
+    {
+        util::CheckGlError("About to bind _vao");
+        glBindVertexArray(_vao);
+        util::CheckGlError("About to draw");
+        glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(_numIndices), GL_UNSIGNED_INT, nullptr);
+        util::CheckGlError("Completed drawing");
+    }
 
-    { { +1, -1, +1 }, { +0, +1, +1 } },
-    { { +1, +1, +1 }, { +0, +1, +1 } },
-    { { -1, +1, +1 }, { +0, +1, +1 } },
+    std::size_t numVertices() const { return _numVertices; }
+    std::size_t numIndices() const { return _numIndices; }
 
-    // -X
-    { { -1, -1, +1 }, { +1, +0, +0 } },
-    { { -1, +1, -1 }, { +1, +0, +0 } },
-    { { -1, -1, -1 }, { +1, +0, +0 } },
+    void update(const std::vector<Vertex>& vertices, const std::vector<GLuint>& indices)
+    {
+        if (_vao == 0) {
+            glGenVertexArrays(1, &_vao);
+        }
+        glBindVertexArray(_vao);
+        _updateVertices(vertices);
+        _updateIndices(indices);
+        glBindVertexArray(0);
+    }
 
-    { { -1, -1, +1 }, { +1, +0, +0 } },
-    { { -1, +1, +1 }, { +1, +0, +0 } },
-    { { -1, +1, -1 }, { +1, +0, +0 } },
+    void update(const std::vector<Vertex>& vertices)
+    {
+        if (_vao == 0) {
+            throw std::runtime_error("Can't call updateVertices() without having already assigned indices");
+        }
+        glBindVertexArray(_vao);
+        _updateVertices(vertices);
+        glBindVertexArray(0);
+    }
 
-    // +Y
-    { { -1, +1, -1 }, { +0, +1, +0 } },
-    { { +1, +1, +1 }, { +0, +1, +0 } },
-    { { +1, +1, -1 }, { +0, +1, +0 } },
+private:
+    void _updateVertices(const std::vector<Vertex>& vertices)
+    {
+        if (vertices.size() > _numVertices) {
+            auto storageSize = static_cast<std::size_t>(vertices.size() * _growthFactor);
+            std::cout << "IndexedVertexStorage::writeVertices resizing Vertex store from " << _numVertices << " to " << storageSize << " to hold " << vertices.size() << " vertices" << std::endl;
+            _numVertices = vertices.size();
 
-    { { -1, +1, -1 }, { +0, +1, +0 } },
-    { { -1, +1, +1 }, { +0, +1, +0 } },
-    { { +1, +1, +1 }, { +0, +1, +0 } },
+            util::CheckGlError("About to create new _vertexVboId");
 
-    // -Y
-    { { -1, -1, -1 }, { +0, +0, +1 } },
-    { { +1, -1, -1 }, { +0, +0, +1 } },
-    { { +1, -1, +1 }, { +0, +0, +1 } },
+            if (_vertexVboId > 0) {
+                glDeleteBuffers(1, &_vertexVboId);
+                _vertexVboId = 0;
+                util::CheckGlError("Deleting old _vertexVboId");
+            }
 
-    { { -1, -1, -1 }, { +0, +0, +1 } },
-    { { +1, -1, +1 }, { +0, +0, +1 } },
-    { { -1, -1, +1 }, { +0, +0, +1 } },
+            glGenBuffers(1, &_vertexVboId);
+            util::CheckGlError("Creating new _vertexVboId");
+
+            glBindBuffer(GL_ARRAY_BUFFER, _vertexVboId);
+            util::CheckGlError("Binding new _vertexVboId");
+
+            glBufferData(
+                GL_ARRAY_BUFFER,
+                sizeof(Vertex) * storageSize,
+                nullptr,
+                GL_STREAM_DRAW);
+            util::CheckGlError("Creating new _vertexVboId");
+
+            glBufferData(
+                GL_ARRAY_BUFFER,
+                sizeof(Vertex) * _numVertices,
+                vertices.data(),
+                GL_STREAM_DRAW);
+            util::CheckGlError("Copying to new _vertexVboId");
+
+            glVertexAttribPointer(
+                static_cast<GLuint>(Vertex::AttributeLayout::Pos),
+                3,
+                GL_FLOAT,
+                GL_FALSE,
+                sizeof(Vertex),
+                (const GLvoid*)offsetof(Vertex, pos));
+            util::CheckGlError("Configuring Vertex::AttributeLayout::Pos");
+            glEnableVertexAttribArray(static_cast<GLuint>(Vertex::AttributeLayout::Pos));
+            util::CheckGlError("Enabling Vertex::AttributeLayout::Pos");
+
+            glVertexAttribPointer(
+                static_cast<GLuint>(Vertex::AttributeLayout::Color),
+                3,
+                GL_FLOAT,
+                GL_TRUE,
+                sizeof(Vertex),
+                (const GLvoid*)offsetof(Vertex, color));
+            util::CheckGlError("Configuring Vertex::AttributeLayout::Color");
+            glEnableVertexAttribArray(static_cast<GLuint>(Vertex::AttributeLayout::Color));
+            util::CheckGlError("Enabling Vertex::AttributeLayout::Color");
+
+        } else {
+            // GPU storage suffices, just copy data over
+            _numVertices = vertices.size();
+            if (_numVertices > 0) {
+                util::CheckGlError("About update _vertexVboId data with glBufferData");
+                glBindBuffer(GL_ARRAY_BUFFER, _vertexVboId);
+                glBufferData(
+                    GL_ARRAY_BUFFER,
+                    sizeof(Vertex) * _numVertices,
+                    vertices.data(),
+                    GL_STREAM_DRAW);
+                util::CheckGlError("Copied _vertexVboId data with glBufferData");
+            }
+        }
+    }
+
+    void _updateIndices(const std::vector<GLuint>& indices)
+    {
+        if (indices.size() > _numIndices) {
+            // GPU storage
+
+            auto storageSize = static_cast<std::size_t>(indices.size() * _growthFactor);
+            std::cout << "IndexedVertexStorage::writeIndices resizing Index store from " << _numIndices << " to " << storageSize << " to hold " << indices.size() << " indices" << std::endl;
+
+            _numIndices = indices.size();
+
+            if (_indexVboId > 0) {
+                glDeleteBuffers(1, &_indexVboId);
+                _indexVboId = 0;
+            }
+
+            glGenBuffers(1, &_indexVboId);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexVboId);
+            glBufferData(
+                GL_ELEMENT_ARRAY_BUFFER,
+                sizeof(GLuint) * storageSize,
+                nullptr,
+                GL_STREAM_DRAW);
+
+            glBufferData(
+                GL_ELEMENT_ARRAY_BUFFER,
+                sizeof(GLuint) * _numIndices,
+                indices.data(),
+                GL_STREAM_DRAW);
+
+            util::CheckGlError("Creating new _indexVboId");
+        } else {
+            // GPU storage suffices, just copy data over
+            _numIndices = indices.size();
+            if (_numIndices > 0) {
+                util::CheckGlError("About to update _indexVboId using glBufferData");
+                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexVboId);
+                glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                    sizeof(GLuint) * _numIndices,
+                    indices.data(),
+                    GL_STREAM_DRAW);
+                util::CheckGlError("Updated _indexVboId using glBufferData");
+            }
+        }
+    }
 };
 
 #pragma mark -
 
 class OpenGLCubeApplication {
 public:
-    void run()
+    OpenGLCubeApplication()
     {
         initWindow();
         initGl();
+    }
 
+    ~OpenGLCubeApplication() = default;
+
+    void run()
+    {
         while (!glfwWindowShouldClose(_window)) {
             glfwPollEvents();
             drawFrame();
@@ -136,24 +271,20 @@ public:
         }
     }
 
-    ~OpenGLCubeApplication()
-    {
-        if (_vboState > 0)
-            glDeleteVertexArrays(1, &_vboState);
-        if (_vertexVboId > 0)
-            glDeleteBuffers(1, &_vertexVboId);
-    }
-
 private:
     GLFWwindow* _window;
 
-    GLuint _vertexVboId = 0;
-    GLuint _vboState = 0;
     ProgramState _program;
+    std::shared_ptr<IndexedVertexStorage> _storage;
 
     mat4 _model = mat4(1);
     mat4 _view = mat4(1);
     mat4 _proj = mat4(1);
+
+    float _createOffset = 0;
+    bool _buildAnother = false;
+    std::vector<Vertex> _vertices;
+    std::vector<GLuint> _indices;
 
 private:
     void initWindow()
@@ -170,6 +301,18 @@ private:
         glfwSetFramebufferSizeCallback(_window, [](GLFWwindow* window, int width, int height) {
             auto app = reinterpret_cast<OpenGLCubeApplication*>(glfwGetWindowUserPointer(window));
             app->onResize(width, height);
+        });
+
+        glfwSetKeyCallback(_window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+            auto app = reinterpret_cast<OpenGLCubeApplication*>(glfwGetWindowUserPointer(window));
+            switch (action) {
+            case GLFW_PRESS:
+                app->onKeyPress(key, scancode, mods);
+                break;
+            case GLFW_RELEASE:
+                app->onKeyRelease(key, scancode, mods);
+                break;
+            }
         });
 
         glfwMakeContextCurrent(_window);
@@ -202,38 +345,7 @@ private:
             throw std::runtime_error("Unable to build program");
         }
 
-        //
-        // create VBOs
-        //
-
-        glGenBuffers(1, &_vertexVboId);
-        glBindBuffer(GL_ARRAY_BUFFER, _vertexVboId);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.size(),
-            Vertices.data(),
-            GL_STATIC_DRAW);
-
-        util::CheckGlError("Creating _vertexVboId");
-
-        glGenVertexArrays(1, &_vboState);
-        glBindVertexArray(_vboState);
-        util::CheckGlError("Creating _vboState");
-
-        glVertexAttribPointer(static_cast<GLuint>(Vertex::AttributeLayout::Pos), 3, GL_FLOAT,
-            GL_FALSE,
-            sizeof(Vertex),
-            (const GLvoid*)offsetof(Vertex, pos));
-
-        glVertexAttribPointer(static_cast<GLuint>(Vertex::AttributeLayout::Color), 3, GL_FLOAT,
-            GL_FALSE,
-            sizeof(Vertex),
-            (const GLvoid*)offsetof(Vertex, color));
-
-        glEnableVertexAttribArray(static_cast<GLuint>(Vertex::AttributeLayout::Pos));
-        glEnableVertexAttribArray(static_cast<GLuint>(Vertex::AttributeLayout::Color));
-        util::CheckGlError("Configuring vertex attrib pointers");
-
-        glBindBuffer(GL_ARRAY_BUFFER, _vertexVboId);
-        glBindVertexArray(0);
+        appendGeometry(vec3(0));
 
         //
         // some constant GL state
@@ -242,7 +354,7 @@ private:
         glClearColor(0.2f, 0.2f, 0.22f, 0.0f);
         glEnable(GL_DEPTH_TEST);
         glDepthFunc(GL_LESS);
-        glEnable(GL_CULL_FACE);
+        glDisable(GL_CULL_FACE);
 
         //
         // force update of the _proj matrix
@@ -260,26 +372,48 @@ private:
         _proj = perspective(radians(FOV_DEGREES), aspect, NEAR_PLANE, FAR_PLANE);
     }
 
+    void onKeyPress(int key, int scancode, int mods)
+    {
+        const int space = glfwGetKeyScancode(GLFW_KEY_SPACE);
+        const int enter = glfwGetKeyScancode(GLFW_KEY_ENTER);
+        if (scancode == space) {
+            _createOffset += 1.1;
+            _buildAnother = true;
+        } else if (scancode == enter) {
+            modifyExistingGeometry();
+        }
+    }
+
+    void onKeyRelease(int key, int scancode, int mods)
+    {
+    }
+
     void drawFrame()
     {
+        if (_buildAnother) {
+            appendGeometry(vec3(_createOffset, 0, 0));
+            _buildAnother = false;
+        }
+
         static auto startTime = std::chrono::high_resolution_clock::now();
         auto currentTime = std::chrono::high_resolution_clock::now();
         float elapsedSeconds = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
 
         //
-        //  Position cube at origin, spinning a bit
+        //  Position target at origin, spinning a bit
         //
-        const float xAngle = elapsedSeconds * M_PI * 0.1F;
-        const float yAngle = elapsedSeconds * M_PI * 0.3F;
-        const float zAngle = elapsedSeconds * M_PI * -0.2F;
-        const vec3 cubePos(0, 0, 0);
-        _model = rotate(rotate(rotate(translate(mat4(1), cubePos), zAngle, vec3(0, 0, 1)), yAngle, vec3(0, 1, 0)), xAngle, vec3(1, 0, 0));
+
+        const float xAngle = elapsedSeconds * M_PI * 0;
+        const float yAngle = elapsedSeconds * M_PI * 0;
+        const float zAngle = elapsedSeconds * M_PI * 0;
+        const vec3 target(0, 0, 0);
+        _model = rotate(rotate(rotate(translate(mat4(1), target), zAngle, vec3(0, 0, 1)), yAngle, vec3(0, 1, 0)), xAngle, vec3(1, 0, 0));
 
         //
         //  Make camera look at cube
         //
 
-        _view = lookAt(vec3(0, 1, -4), cubePos, vec3(0, 1, 0));
+        _view = lookAt(vec3(0, 1, -4), target, vec3(0, 1, 0));
         const mat4 mvp = _proj * _view * _model;
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -287,8 +421,48 @@ private:
         glUseProgram(_program.program);
         glUniformMatrix4fv(_program.uniformLocMVP, 1, GL_FALSE, value_ptr(mvp));
 
-        glBindVertexArray(_vboState);
-        glDrawArrays(GL_TRIANGLES, 0, static_cast<GLsizei>(Vertices.size()));
+        _storage->draw();
+    }
+
+    void appendGeometry(vec3 offset)
+    {
+        auto vertices = std::vector<Vertex> {
+            { { -0.5f, -0.5f, 0 }, { 1.0f, 0.0f, 0.0f } },
+            { { 0.5f, -0.5f, 0 }, { 0.0f, 1.0f, 0.0f } },
+            { { 0.5f, 0.5f, 0 }, { 0.0f, 0.0f, 1.0f } },
+            { { -0.5f, 0.5f, 0 }, { 1.0f, 1.0f, 1.0f } }
+        };
+
+        for (auto& v : vertices) {
+            v.pos += offset;
+        }
+
+        auto indices = std::vector<GLuint> {
+            0, 1, 2, 2, 3, 0
+        };
+
+        GLuint indexOffset = static_cast<GLuint>(_vertices.size());
+        for (auto& i : indices) {
+            i += indexOffset;
+        }
+
+        _vertices.insert(_vertices.end(), vertices.begin(), vertices.end());
+        _indices.insert(_indices.end(), indices.begin(), indices.end());
+
+        if (_storage) {
+            _storage->update(_vertices, _indices);
+        } else {
+            _storage = std::make_shared<IndexedVertexStorage>(_vertices, _indices);
+        }
+    }
+
+    void modifyExistingGeometry()
+    {
+        for (auto& v : _vertices) {
+            v.pos.x += 0.1f;
+        }
+
+        _storage->update(_vertices);
     }
 };
 
